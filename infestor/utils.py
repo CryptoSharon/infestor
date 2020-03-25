df24e49727d1232f46b91ed769676c7f689aff7d
CryptoSharon <35318508+CryptoSharon@users.noreply.github.com>
2020-03-25 19:10:18 -0400

    Update utils.py

--- infestor/utils.py
+++ infestor/utils.py
@@ -24,7 +24,7 @@ def generate_keys(new_account_name, master_key, exclude_master=False):
 
 def username_is_valid(username):
     # regex pattern is found at
-    # https://steemit.com/programming/@cryptosharon/the-5-rules-of-a-valid-username-on-the-steem-blockchain-and-a-3-sbd-contest-to-make-an-account-name-validation-regex#@cryptosharon/re-eonwarped-re-cryptosharon-re-eonwarped-re-cryptosharon-re-eonwarped-re-cryptosharon-re-artopium-re-cryptosharon-the-5-rules-of-a-valid-username-on-the-steem-blockchain-and-a-3-sbd-contest-to-make-an-account-name-validation-regex-20180313t214044982z
+    # https://peakd.com/programming/@cryptosharon/the-5-rules-of-a-valid-username-on-the-steem-blockchain-and-a-3-sbd-contest-to-make-an-account-name-validation-regex#@cryptosharon/re-eonwarped-re-cryptosharon-re-eonwarped-re-cryptosharon-re-eonwarped-re-cryptosharon-re-artopium-re-cryptosharon-the-5-rules-of-a-valid-username-on-the-steem-blockchain-and-a-3-sbd-contest-to-make-an-account-name-validation-regex-20180313t214044982z
     return bool(re.search('^[a-z](-[a-z0-9](-[a-z0-9])*)?(-[a-z0-9]|[a-z0-9])'
                           '*(?:\.[a-z](-[a-z0-9](-[a-z0-9])*)?(-[a-z0-9]|'
                           '[a-z0-9])*)*$', username))
